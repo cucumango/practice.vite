b01990bbb3b2d85e598db19873b6ba00e77d3fe8
Byungjin Park <bjpark0813@gmail.com>
2024-04-20 01:44:09 +0900

    add initial code

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Container from '@mui/material/Container';
+
+const App = () => {
+    return (<Container maxWidth='xl'>hello</Container>);
+};
+
+export default App;
